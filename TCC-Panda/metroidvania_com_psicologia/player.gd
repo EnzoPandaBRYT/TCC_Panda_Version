66b93b112aab406380eb71a348e66869448484d9
PandaBrasileiro <enzocargame@gmail.com>
2025-05-11 00:01:22 -0300

    Adicionado Sistema de Inventário

--- TCC-Panda/metroidvania_com_psicologia/player.gd
+++ TCC-Panda/metroidvania_com_psicologia/player.gd
@@ -8,14 +8,17 @@ extends CharacterBody2D
 const SPEED = 300.0
 
 # Velocidade do Pulo do Jogador
-const JUMP_VELOCITY = -400.0
+const jump_speed = -400.0
 
 # Estabelece o número máximo de Pulos Máximos do jogador
 # De início, o número de pulos máximos é 1
-var maxJumps = 1
+var maxJumps = 3
 
 # Pulo Duplo:
 var doubleJump = false
+
+# Pulo Triplo:
+var tripleJump = false
 
 # Pulando
 var isJumping = false
@@ -35,23 +38,28 @@ var twReloading = false
 
 # Processo executado a CADA FRAME possível do jogo
 func _physics_process(delta: float) -> void:
-	
+	if is_on_floor():
+		isJumping = false
 	
 	# Adiciona a gravidade - Padrão: 980.0 px/s
 	if not is_on_floor():
-		isJumping = false # Verifica se o jogador está pulando
 		velocity += get_gravity() * delta # Velocidade de gravidade do projeto (Padrão) * Frames possíveis
 	
 	if is_on_floor() and doubleJump == true:
 		maxJumps = 2 # Pulos máximos = 2 (Apenas para verificação)
 	
+	if is_on_floor() and tripleJump == true:
+		maxJumps = 3 # Pulos máximos = 2 (Apenas para verificação)
+	
 	# DEV_TOOL = HOME no teclado; Libera todo tipo de mecânica possível
-	if Input.is_action_just_pressed("DEV_TOOL") and is_on_floor():
+	if Input.is_action_just_pressed("DEV_TOOL"):
 		doubleJump = true # Libera o Pulo Duplo e Time Warp (DEV TOOL)
+		tripleJump = true
 		timeWarp = true
-		print("Double Jump; Time Warp desbloqueado!")
+		print("Double Jump e Triple Jump; Time Warp desbloqueado!")
 		
 		print("- Double Jump = Pule no AR!")
+		print("- Triple Jump = Pule no AR!... TRÊS VEZES!")
 		print("- Time Warp = Pressione L para diminuir o FLUXO TEMPORAL")
 	
 	# Tecla L pressionada ativa o Time Warp
@@ -64,19 +72,20 @@ func _physics_process(delta: float) -> void:
 	
 	# Verifica a tecla de pulo
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		print(maxJumps)
 		isJumping = true # Atualiza o verificador de pulo
-		velocity.y = JUMP_VELOCITY # Utiliza velocidade para não "teleportar" o jogador para cima
+		velocity.y = jump_speed # Utiliza velocidade para não "teleportar" o jogador para cima
 	
 	# Permite o Pulo Duplo, se liberado
-	if Input.is_action_just_pressed("jump") and not is_on_floor() and maxJumps == 2:
-		velocity.y = JUMP_VELOCITY * 0.8 # Indica que o segundo pulo será menor
+	if Input.is_action_just_pressed("jump") and not is_on_floor() and maxJumps > 1:
+		isJumping = true
+		velocity.y = jump_speed * 0.9 # Indica que o segundo pulo será menor
 		maxJumps = 0 # Zera o contador de pulos máximos para garantir que o jogador não irá sair voando
 	
 	# Detecta a ação de STOMPAR
 	if Input.is_action_just_pressed("stomp") and not is_on_floor():
-		velocity += get_gravity() * 0.5 # Ação de descer mais rápido, diminui o efeito da gravidade para melhores efeitos
-		anim.flip_v = true
-		anim.play("jump")
+		velocity += get_gravity() * 0.6 # Ação de descer mais rápido, diminui o efeito da gravidade para melhores efeitos
+
 
 	# Pega o INPUT da direção do jogador e com isso, o movimenta pelo cenário
 	var direction := Input.get_axis("move_left", "move_right")
@@ -93,11 +102,14 @@ func _physics_process(delta: float) -> void:
 		elif direction < 0 and not isJumping:
 			anim.flip_h = true # Olhando para a esquerda
 			anim.play("moving") # Toca a animação de Mover, porém espelhada
-		elif isJumping:
-			anim.flip_v = false # Determina que não é para virar o sprite de pulo
-			anim.play("jump") # Toca a animação de Pulo; A verificação do pulo é feita na ação do pulo
 		else:
 			anim.play("idle") # Caso nenhuma das animações acima seja executada, entrará em estado "inerte"
+	else:
+		if velocity.y < 0.0:
+			anim.flip_v = false # Subindo
+		elif velocity.y > 0.0:
+			anim.flip_v = true # Caindo
+		anim.play("jump") # Toca a animação de Pulo; A verificação do pulo é feita na ação do pulo
 	move_and_slide()
 
 func slow_motion(duration: float = 1.5):
